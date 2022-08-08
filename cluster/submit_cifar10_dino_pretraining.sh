13d1b6a9cd78cfd3aca131a678bd430ff3d93fa9
ferreirafabio <ferreirafabio@users.noreply.github.com>
2022-08-08 02:34:53 +0200

    fixed port issue outside

--- cluster/submit_cifar10_dino_pretraining.sh
+++ cluster/submit_cifar10_dino_pretraining.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH -p alldlc_gpu-rtx2080
-##SBATCH -q dlc-wagnerd
 #SBATCH --gres=gpu:1
 #SBATCH -J CIFAR-10_PT_DINO
 #SBATCH -t 23:59:59
 
 pip list
 
+source /home/ferreira/.profile
 source activate dino
 
-port=`python find_free_port.py`
+port=`python cluster/find_free_port.py`
 echo "found free port $port"
 
-python -m torch.distributed.launch --master_port=$port --nproc_per_node=1 main_dino.py --arch vit_small --output_dir /work/dlclarge2/wagnerd-metassl-experiments/dino/CIFAR-10/$EXPERIMENT_NAME --batch_size_per_gpu 64 --saveckp_freq 10 --gpu 1 --world_size 1 --dataset CIFAR-10 --epochs 800 --seed $SEED
+python -m torch.distributed.launch --master_port=$port --nproc_per_node=1 main_dino.py --arch vit_small --output_dir /work/dlclarge2/ferreira-dino/metassl-dino/experiments/$EXPERIMENT_NAME --batch_size_per_gpu 64 --saveckp_freq 10 --gpu 1 --world_size 1 --dataset CIFAR-10 --epochs 800 --seed $SEED
