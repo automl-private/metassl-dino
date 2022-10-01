e8a6b793d66112672b066e13604c8a6bb2f1dff4
Diane Wagner <wagnerd@informatik.uni-freiburg.de>
2022-10-01 14:26:21 +0200

    Update submission scripts (Diane)

--- cluster/submit_imagenet_dino_neps_data_augmentation.sh
+++ cluster/submit_imagenet_dino_neps_data_augmentation.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
-#SBATCH -p mldlc_gpu-rtx2080
+#SBATCH -p alldlc_gpu-rtx2080
 ##SBATCH -q dlc-wagnerd
 #SBATCH --gres=gpu:8
 #SBATCH -J IN_DA
-#SBATCH -t 3-23:59:59
-#SBATCH --array 0-24%5
+##SBATCH -t 3-23:59:59
+#SBATCH -t 00:29:59
+##SBATCH --array 0-24%10
 
 pip list
 
-export PATH="/home/stolld/.conda/bin:$PATH"
+# DANNY
+# export PATH="/home/stolld/.conda/bin:$PATH"
 
-mkdir -p /work/dlclarge1/stolld-metassl_dino/dino/ImageNet/$EXPERIMENT_NAME/dino_communication
-filename=/work/dlclarge1/stolld-metassl_dino/dino/ImageNet/$EXPERIMENT_NAME/dino_communication/$(openssl rand -hex 12)
+# mkdir -p /work/dlclarge1/stolld-metassl_dino/dino/ImageNet/$EXPERIMENT_NAME/dino_communication
+# filename=/work/dlclarge1/stolld-metassl_dino/dino/ImageNet/$EXPERIMENT_NAME/dino_communication/$(openssl rand -hex 12)
 
-python -m torch.distributed.launch --use_env --nproc_per_node=8 --nproc_per_node=1 main_dino.py --config_file_path $filename --arch vit_small --data_path /data/datasets/ImageNet/imagenet-pytorch/train --output_dir /work/dlclarge1/stolld-metassl_dino/dino/ImageNet/$EXPERIMENT_NAME --batch_size_per_gpu 32 --saveckp_freq 10 --seed $SEED --is_neps_run
+# python -m torch.distributed.launch --use_env --nproc_per_node=8 --nproc_per_node=1 main_dino.py --config_file_path $filename --arch vit_small --data_path /data/datasets/ImageNet/imagenet-pytorch/train --output_dir /work/dlclarge1/stolld-metassl_dino/dino/ImageNet/$EXPERIMENT_NAME --batch_size_per_gpu 32 --saveckp_freq 10 --seed $SEED --is_neps_run
+
+source activate dino
+
+# DIANE
+mkdir -p /work/dlclarge2/wagnerd-metassl-experiments/dino/ImageNet/$EXPERIMENT_NAME/dino_communication
+filename=/work/dlclarge2/wagnerd-metassl-experiments/dino/ImageNet/$EXPERIMENT_NAME/dino_communication/$(openssl rand -hex 12)
+
+python -m torch.distributed.launch --use_env --nproc_per_node=8 --nproc_per_node=1 main_dino.py --config_file_path $filename --arch vit_small --data_path /data/datasets/ImageNet/imagenet-pytorch/train --output_dir /work/dlclarge2/wagnerd-metassl-experiments/dino/ImageNet/$EXPERIMENT_NAME --batch_size_per_gpu 32 --saveckp_freq 10 --seed $SEED --is_neps_run \
+	--train_dataset_percentage_usage 1.0 --valid_size 0.1
