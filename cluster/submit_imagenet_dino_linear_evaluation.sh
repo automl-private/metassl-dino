45c1411d977a420436ee3ffc8fe277b62dec9f1e
ferreirafabio <ferreirafabio@users.noreply.github.com>
2022-10-01 19:46:09 +0200

    added justfile

--- cluster/submit_imagenet_dino_linear_evaluation.sh
+++ cluster/submit_imagenet_dino_linear_evaluation.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-#SBATCH -p alldlc_gpu-rtx2080
-##SBATCH -q dlc-wagnerd
+#SBATCH -p alldlc_gpu-rtx2080 #mldlc_gpu-rtx2080 #bosch_gpu-rtx2080 #alldlc_gpu-rtx2080
 #SBATCH --gres=gpu:8
-#SBATCH -J IN_EVAL_DINO
+#SBATCH -J IN_EVAL_DINO_C29_BS40
 #SBATCH -t 23:59:59
 
 pip list
 
+source /home/ferreira/.profile
 source activate dino
 
-
-python -m torch.distributed.launch --nproc_per_node=8 eval_linear.py --data_path /data/datasets/ImageNet/imagenet-pytorch --output_dir /work/dlclarge2/wagnerd-metassl-experiments/dino/ImageNet/$EXPERIMENT_NAME --batch_size_per_gpu 32 --pretrained_weights /work/dlclarge2/wagnerd-metassl-experiments/dino/ImageNet/$EXPERIMENT_NAME/checkpoint.pth --seed $SEED
+python -m torch.distributed.launch --nproc_per_node=8 --nnodes=1 eval_linear.py --data_path /data/datasets/ImageNet/imagenet-pytorch --output_dir /work/dlclarge2/ferreira-dino/metassl-dino/experiments/$EXPERIMENT_NAME --pretrained_weights /work/dlclarge2/ferreira-dino/metassl-dino/experiments/neps_imagenet_balanced_val_data_augmentation/results/config_29/checkpoint.pth --seed $SEED --dataset ImageNet --batch_size_per_gpu 40
--- cluster/submit_imagenet_dino_pretraining.sh
+++ cluster/submit_imagenet_dino_pretraining.sh
@@ -11,4 +11,3 @@ source activate dino
 
 python -u -m torch.distributed.launch --use_env --nproc_per_node=8 --nnodes 1 main_dino.py --arch vit_small --data_path /data/datasets/ImageNet/imagenet-pytorch/train --output_dir /work/dlclarge2/wagnerd-metassl-experiments/dino/ImageNet/$EXPERIMENT_NAME --batch_size_per_gpu 32 --saveckp_freq 10 --seed $SEED
 
-
