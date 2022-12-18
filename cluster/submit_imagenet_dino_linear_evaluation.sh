d27d677fb4678d772b023f31f94792b1e1c4608f
Diane Wagner <wagnerd@informatik.uni-freiburg.de>
2022-12-18 20:09:34 +0100

    Update submission scripts

--- cluster/submit_imagenet_dino_linear_evaluation.sh
+++ cluster/submit_imagenet_dino_linear_evaluation.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 #SBATCH -p alldlc_gpu-rtx2080 #mldlc_gpu-rtx2080 #bosch_gpu-rtx2080 #alldlc_gpu-rtx2080
 #SBATCH --gres=gpu:8
-#SBATCH -J IN_EVAL_DINO_C29_BS40
+#SBATCH -J IN_EVAL_TV2_DINO_Baseline
 #SBATCH -t 23:59:59
 
 pip list
 
-source /home/ferreira/.profile
 source activate dino
 
 # python -m torch.distributed.launch --nproc_per_node=8 --nnodes=1 eval_linear.py --data_path /data/datasets/ImageNet/imagenet-pytorch --output_dir /work/dlclarge2/ferreira-dino/metassl-dino/experiments/$EXPERIMENT_NAME --pretrained_weights /work/dlclarge2/ferreira-dino/metassl-dino/experiments/neps_imagenet_balanced_val_data_augmentation/results/config_29/checkpoint.pth --seed $SEED --dataset ImageNet --batch_size_per_gpu 40
 
 # From PT with same experiment name
-python -m torch.distributed.launch --nproc_per_node=8 eval_linear.py --data_path /data/datasets/ImageNet/imagenet-pytorch --output_dir /work/dlclarge2/wagnerd-metassl-experiments/dino/ImageNet/$EXPERIMENT_NAME --batch_size_per_gpu 32 --pretrained_weights /work/dlclarge2/wagnerd-metassl-experiments/dino/ImageNet/$EXPERIMENT_NAME/checkpoint.pth --seed $SEED
+# python -m torch.distributed.launch --nproc_per_node=8 eval_linear.py --data_path /data/datasets/ImageNet/imagenet-pytorch --output_dir /work/dlclarge2/wagnerd-metassl-experiments/dino/ImageNet/$EXPERIMENT_NAME --batch_size_per_gpu 32 --pretrained_weights /work/dlclarge2/wagnerd-metassl-experiments/dino/ImageNet/$EXPERIMENT_NAME/checkpoint.pth --seed $SEED
 
 # From NePS run
-# python -m torch.distributed.launch --nproc_per_node=8 eval_linear.py --data_path /data/datasets/ImageNet/imagenet-pytorch --output_dir /work/dlclarge2/wagnerd-metassl-experiments/dino/ImageNet/$EXPERIMENT_NAME --batch_size_per_gpu 32 --pretrained_weights /work/dlclarge2/ferreira-dino/metassl-dino/experiments/neps_imagenet_balanced_val_data_augmentation_new_fixed_balanced_val_set/results/config_4/checkpoint.pth --seed $SEED
+# python -m torch.distributed.launch --nproc_per_node=8 eval_linear.py --data_path /data/datasets/ImageNet/imagenet-pytorch --output_dir /work/dlclarge2/wagnerd-metassl-experiments/dino/ImageNet/$EXPERIMENT_NAME --batch_size_per_gpu 32 --pretrained_weights /work/dlclarge2/ferreira-dino/metassl-dino/experiments/neps_imagenet_balanced_val_data_augmentation_new_fixed_balanced_val_set_backup_31_Oct_2022/results/config_23/checkpoint.pth --seed $SEED
+
+# From Baseline
+# python -m torch.distributed.launch --nproc_per_node=8 eval_linear.py --data_path /data/datasets/ImageNet/imagenet-pytorch --output_dir /work/dlclarge2/wagnerd-metassl-experiments/dino/ImageNet/$EXPERIMENT_NAME --batch_size_per_gpu 32 --pretrained_weights /work/dlclarge2/wagnerd-metassl-experiments/dino/ImageNet/22-08-05_baseline_vit_seed0/checkpoint.pth --seed $SEED
+
+# From DA config
+python -m torch.distributed.launch --nproc_per_node=8 eval_linear.py --data_path /data/datasets/ImageNet/imagenet-pytorch --output_dir /work/dlclarge2/wagnerd-metassl-experiments/dino/ImageNet/$EXPERIMENT_NAME --batch_size_per_gpu 32 --pretrained_weights /work/dlclarge2/wagnerd-metassl-experiments/dino/ImageNet/22-12-01_DA_config17_backup_pre-training_seed0/checkpoint.pth --seed $SEED
+
+# From Baseline - val
+# python -m torch.distributed.launch --nproc_per_node=8 eval_linear.py --data_path /data/datasets/ImageNet/imagenet-pytorch --output_dir /work/dlclarge2/wagnerd-metassl-experiments/dino/ImageNet/$EXPERIMENT_NAME --batch_size_per_gpu 32 --pretrained_weights /work/dlclarge1/stolld-metassl_dino/dino/ImageNet/22-09-22_baseline_balanced_val_pt+eval_seed0/checkpoint.pth --seed $SEED
--- cluster/submit_imagenet_dino_neps_training.sh
+++ cluster/submit_imagenet_dino_neps_training.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 #SBATCH -p mldlc_gpu-rtx2080
-##SBATCH -q dlc-wagnerd
 #SBATCH --gres=gpu:8
-#SBATCH -J IN_TR
+#SBATCH -J IN_TR_NePS
 #SBATCH -t 3-23:59:59
-#SBATCH --array 0-24%5
+#SBATCH --array 0-24%1
 
+# DANNY
+# export PATH="/home/stolld/.conda/bin:$PATH"
+# pip list
+# mkdir -p /work/dlclarge1/stolld-metassl_dino/dino/ImageNet/$EXPERIMENT_NAME/dino_communication
+# filename=/work/dlclarge1/stolld-metassl_dino/dino/ImageNet/$EXPERIMENT_NAME/dino_communication/$(openssl rand -hex 12)
+
+# python -m torch.distributed.launch --use_env --nproc_per_node=8 --nproc_per_node=1 main_dino.py --config_file_path $filename --arch vit_small --data_path /data/datasets/ImageNet/imagenet-pytorch/train --output_dir /work/dlclarge1/stolld-metassl_dino/dino/ImageNet/$EXPERIMENT_NAME --batch_size_per_gpu 32 --saveckp_freq 10 --seed $SEED --is_neps_run
+
+
+# DIANE
+source activate dino
 pip list
+mkdir -p /work/dlclarge2/wagnerd-metassl-experiments/dino/ImageNet/$EXPERIMENT_NAME/dino_communication
+filename=/work/dlclarge2/wagnerd-metassl-experiments/dino/ImageNet/$EXPERIMENT_NAME/dino_communication/$(openssl rand -hex 12)
 
-export PATH="/home/stolld/.conda/bin:$PATH"
+python -m torch.distributed.launch --use_env --nproc_per_node=8 main_dino.py --config_file_path $filename --arch vit_small --data_path /data/datasets/ImageNet/imagenet-pytorch/train --output_dir /work/dlclarge2/wagnerd-metassl-experiments/dino/ImageNet/$EXPERIMENT_NAME --batch_size_per_gpu 32 --saveckp_freq 10 --seed $SEED --is_neps_run --config_space training \
+# --train_dataset_percentage_usage 1.0 --valid_size 0.1
 
-mkdir -p /work/dlclarge1/stolld-metassl_dino/dino/ImageNet/$EXPERIMENT_NAME/dino_communication
-filename=/work/dlclarge1/stolld-metassl_dino/dino/ImageNet/$EXPERIMENT_NAME/dino_communication/$(openssl rand -hex 12)
 
-python -m torch.distributed.launch --use_env --nproc_per_node=8 --nproc_per_node=1 main_dino.py --config_file_path $filename --arch vit_small --data_path /data/datasets/ImageNet/imagenet-pytorch/train --output_dir /work/dlclarge1/stolld-metassl_dino/dino/ImageNet/$EXPERIMENT_NAME --batch_size_per_gpu 32 --saveckp_freq 10 --seed $SEED --is_neps_run --config_space training
